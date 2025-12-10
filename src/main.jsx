97a94310ca9806fbfdd6185169d5f9df2bd0a7ba
medié <danimedie@gmail.com>
2025-12-10 22:31:19 +0100

    setup styles with tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+
+import '@fontsource/nunito/600.css';
+import '@fontsource/nunito/700.css';
+import '@fontsource/nunito/800.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
